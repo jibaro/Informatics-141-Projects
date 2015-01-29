4c2f09c3c7fead6fd55d3c72efeb4bcc1befc924
yoshiscienceguy <fdepaz@uci.edu>
2015-01-28 17:23:51 -0800

    Got it working
    yay :D

---
+++ Project 3/src/ir/assignments/three/Frequency.java
@@ -0,0 +1,36 @@
+//Fernando de Paz
+//81962579
+//Assignment 2 ICS 121 
+package ir.assignments.three;
+
+public final class Frequency {
+	private final String word;
+	private int frequency;
+	
+	public Frequency(String word) {
+		this.word = word;
+		this.frequency = 0;
+	}
+	
+	public Frequency(String word, int frequency) {
+		this.word = word;
+		this.frequency = frequency;
+	}
+	
+	public String getText() {
+		return word;
+	}
+	
+	public int getFrequency() {
+		return frequency;
+	}
+	
+	public void incrementFrequency() {
+		frequency++;
+	}
+	
+	@Override
+	public String toString() {
+		return word + ":" + frequency;
+	}
+}
---
+++ Project 3/src/ir/assignments/three/WordFrequencyCounter.java
@@ -0,0 +1,47 @@
+//Fernando de Paz
+//81962579
+//Assignment 2 ICS 121 
+package ir.assignments.three;
+
+import ir.assignments.three.Frequency;
+import ir.assignments.three.Utilities;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class WordFrequencyCounter {
+
+	private WordFrequencyCounter() {}
+
+	public static List<Frequency> computeWordFrequencies(List<String> words) {
+
+		List<Frequency> frequency = new ArrayList<Frequency>();
+		//Go through tokens and check if they exist, if it does update counter
+		for (String token : words){
+			boolean exists = false;
+			for(Frequency word: frequency){
+				if(token.equals(word.getText())){
+					word.incrementFrequency();
+					exists = true;
+					break;
+				}
+			}
+			//if they don't exist create newword of type Frequency and add it to frequency list
+			if(exists == false){
+				Frequency newword = new Frequency(token,1);
+				frequency.add(newword);
+			}
+			
+		}
+		return frequency;
+	}
+	
+
+	public static void main(String[] args) {
+		File file = new File(args[0]);
+		List<String> words = Utilities.tokenizeFile(file);
+		List<Frequency> frequencies = computeWordFrequencies(words);
+		Utilities.printFrequencies(frequencies);
+	}
+}
